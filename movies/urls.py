427c3cd373d41f1e7dbb359f4df38a75d04b08f6
Rick <romeo2310@yandex.ru>
2020-05-08 17:07:45 +0300

    Добавление отзывов

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path('', views.MoviesView.as_view(), name='movie_list'),
 	path('<slug:slug>/', views.MovieDetail.as_view(), name='movie_detail'),
+	path('review/<int:pk>/', views.AddReview.as_view(), name='add_review'),
 ]
